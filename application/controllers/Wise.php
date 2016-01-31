8837694b4eaeee954b18fe202e5fba99f5884cd3
alexander-kyle <alexander.kyle@gmail.com>
2016-01-30 17:53:31 -0800

    Link 8 Working Signed-off-by:alexander-kyle <alexander.kyle@gmail.com>

---
+++ application/controllers/Wise.php
@@ -0,0 +1,24 @@
+<?php
+
+class Wise extends Application {
+
+	function __construct()
+	{
+		parent::__construct();
+	}
+
+	//-------------------------------------------------------------
+	//  The normal pages
+	//-------------------------------------------------------------
+
+	
+        function Bingo(){
+           $this->data['pagebody'] = 'justone';	// this is the view we want shown
+		// build the list of authors, to pass on to our view
+		$record = $this->quotes->get(6);
+		$this->data = array_merge($this->data, $record);
+
+		$this->render(); 
+            
+        }
+        }
